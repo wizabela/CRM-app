a11428901ad59e15fa7ca3266df9d7c61da0edfa
id github: wizabela <izabela.wyrebska@gmail.com>
2021-11-06 16:13:26 +0100

    init

---
+++ notes.js
@@ -0,0 +1,8 @@
+/**
+ * BAZA KLIENTÓW CRM
+ *
+ * Zadanie to stworzyć bazę klientów.
+ * Będą w niech przechowywane imiona, maile, notatki i informacje o następnej dacie kontaktu.
+ * Można wyświetlać, dodawać, usuwać, modyfikować rekordy.
+ *
+ */
